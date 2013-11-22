db280625222b184296a9f09677d2432d25ba676f
samuel villarreal <sigmaxz@users.noreply.github.com>
2013-11-22 00:55:58 -0800

    Create commandex1.cc

---
+++ commandex1.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+void command_line(int argc, char ** argv, string & file1 , string & file2, int & k_organisms, int & flag )
+{
+
+	file1 = "input.txt";
+	file2 = "query.txt";
+	k_organisms = 100;
+	flag = 1;
+
+	for(int i = 1; i < argc; i++)
+	{
+			if(*(argv[i]) == 'd')
+			{
+				file1 = argv[i+2];
+			}
+			else if(*(argv[i]) == 'q')
+			{
+				file2 = argv[i+2];
+			}
+			else if(*(argv[i]) == 'n')
+			{
+				string ks = (argv[i+2]);
+				k_organisms = atoi(ks.c_str());
+			}
+			else if(*(argv[i]) == 'a')
+			{
+				string fs = (argv[i+2]);
+				flag = atoi(fs.c_str());
+			}
+			else
+				cout << "error" << endl;
+	
+			i = i+2;
+	}
+
+}
+int main( int argc, char** argv)
+{
+	string file1,file2;
+	int k_organisms, flag;	
+	command_line(argc, argv, file1, file2, k_organisms, flag);
+	cout << file1 << " " << file2 << " " << k_organisms << " " << flag;
+
+	return 0;
+}
